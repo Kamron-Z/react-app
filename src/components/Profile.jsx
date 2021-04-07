f3cef116eb81c1735222b0069df3298ed8e88eac
Kamron-Z <komikomi112020@gmail.com>
2021-04-07 14:53:31 +0500

    first commit

---
+++ src/components/Profile.jsx
@@ -0,0 +1,26 @@
+const Profile = () => {
+   return (
+      <div className='content'>
+         <div>
+            <img className='content__img' src='https://images.ctfassets.net/hrltx12pl8hq/4plHDVeTkWuFMihxQnzBSb/aea2f06d675c3d710d095306e377382f/shutterstock_554314555_copy.jpg' />
+         </div>
+         <div>
+            avatar + info
+        </div>
+         <div>
+            my post
+        </div>
+         <div>
+            new post
+        </div>
+         <div>
+            post 1
+        </div>
+         <div>
+            post 1
+        </div>
+      </div>
+   );
+}
+
+export default Profile;
